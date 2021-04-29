62728798ed1ecbc36e50ebff565bf4d53d8e210f
franciCabrera <francabre97@gmail.com>
2021-04-29 13:08:07 -0300

    Agrego parte dos y comento el codigo

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 const btnAgregarCaja = document.getElementById("btnAgregarCaja"); //Trae el evento click
 const divContenedor = document.getElementById("contenedor"); //Trae el div contenedor
 const input = document.getElementById("input-palabra"); //Trae lo que ingresamos en el input
-
-console.log(btnAgregarCaja);
+const imagen = document.getElementById("input-link"); // Trae el link de la imagen
 
 function reset(){
     input.value = '';
+    imagen.value = '';
 }
 
+console.log(btnAgregarCaja);
+
 btnAgregarCaja.addEventListener("click" , function(){
+    //Creamos los Elementos
     const nuevaCaja = document.createElement("div");
+    const nuevaImg = document.createElement("img");
     const nuevoP = document.createElement("p");
-    
+    //Tomo el valor del P y de la Imagen
+    nuevaImg.setAttribute('src', imagen.value);
     nuevoP.textContent = input.value;
-
+    //Creo nuevas clases
     nuevaCaja.classList.add("caja");
-
-    nuevaCaja.appendChild(nuevoP);
-    divContenedor.appendChild(nuevaCaja);
-    reset();
+    nuevaImg.classList.add("imag");
+    //Muestro la caja creada
+    nuevaCaja.appendChild(nuevaImg); // Agrega la Img a la caja
+    nuevaCaja.appendChild(nuevoP); // Agrega el P a la caja
+    divContenedor.appendChild(nuevaCaja); // Agrega la caja al div
+    //Reseteo los input
+    reset(); //Es una funcion llamada arriba
 });
